6d5ef8f8c97d680f2a39ee2c83092cc6f8dc21ba
Christoffer Krath <chkra19@student.sdu.dk>
2020-11-18 10:35:52 +0100

    Update BuildingManagementSystem.java

--- src/com/company/BuildingManagementSystem.java
+++ src/com/company/BuildingManagementSystem.java
@@ -1,8 +1,30 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class BuildingManagementSystem
 {
     private ArrayList<Building> Buildings = new ArrayList<Building>();
+
+    public BuildingManagementSystem (ArrayList Buildings)
+    {
+        this.Buildings = Buildings;
+    }
+
+    protected void AddBuilding(Building b)
+    {
+        Buildings.add(b);
+    }
+
+    protected void RemoveBuilding(Building b, UUID id)
+    {
+        if( b.getID() == id )
+        Buildings.remove(b);
+    }
+
+    protected void DeleteBuildings()
+    {
+        Buildings.clear();
+    }
 }
