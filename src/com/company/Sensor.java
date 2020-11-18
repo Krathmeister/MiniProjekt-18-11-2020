4895cc3bff57c7e74cfa6b61c8e8880c9ac90ef2
Mathias Nielsen <mnhnielsen@gmail.com>
2020-11-18 10:27:04 +0100

    Update

--- src/com/company/Sensor.java
+++ src/com/company/Sensor.java
@@ -2,18 +2,15 @@ package com.company;
 
 import java.util.UUID;
 
-public class Sensor extends Unit
-{
+public class Sensor extends Unit {
     private double value;
 
-    public Sensor (String name, UUID ID, double value)
-    {
+    public Sensor(String name, UUID ID, double value) {
         super(name, ID);
         this.value = value;
     }
 
-    public double getValue()
-    {
+    public double getValue() {
         return value;
     }
 }
